d673dd7130009635892313b190fa9ede75952f90
Ahmed Elbatt <ahmed.elbatt@vexcash.com>
2025-01-16 20:14:54 +0100

    Add Identity Tables

--- src/Persistance/ApplicationDbContext.cs
+++ src/Persistance/ApplicationDbContext.cs
@@ -1,7 +1,8 @@
-﻿using System.Reflection;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Reflection;
 
 namespace Persistance;
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
     public DbSet<Poll> Polls { get; set; }
 
---
+++ src/Persistance/EntitiesConfigurations/ApplicationUserConfigurations.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Persistance.EntitiesConfigurations;
+public class ApplicationUserConfigurations : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.Property(x => x.FirstName).HasMaxLength(100);
+        builder.Property(x => x.LastName).HasMaxLength(100);
+    }
+}
